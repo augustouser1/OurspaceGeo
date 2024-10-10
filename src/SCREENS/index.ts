6da69cf2776f6df66aeb1ffe463f9f20685453ac
augustouser1 <augustocsbitencourt8@gmail.com>
2024-10-10 09:44:25 -0300

    projeto

---
+++ src/SCREENS/index.ts
@@ -0,0 +1,2 @@
+export {QrCode as ScreenQrCode} from "./QrCode"
+export {LocationMap as ScreenLocation } from "./LocationMap"
---
+++ src/components/Album/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    albumContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    albumAssetsContainer:{
+       
+    }
+})
